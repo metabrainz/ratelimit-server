a24bf14ec0f68820de03738b7b36d17c58ad3d31
Dave Evans <git@djce.org.uk>
2011-12-24 11:47:37 +0000

    First commit
    ratelimit-server now lives in this repo, although it's also in
    musicbrainz-server.

---
+++ README.txt
@@ -0,0 +1,76 @@
+Execution
+=========
+
+bind addr and port are specified in the ./run script (arguments to ratelimit-server).
+
+Logs
+====
+
+"tail -F -s 0.1 log/main/current | tain64local" - tail the busy log (all
+requests/responses)
+
+"tail -F -s 0.1 log/quiet/current | tain64local" - tail the quiet log
+(excludes requests/responses, so this is just start/stop/error, etc)
+
+Monitoring
+==========
+
+"./top" - every 10 seconds, shows the most-requested keys, their rates,
+limits, what proportion of requests are being denied, etc.
+
+Debugging
+=========
+
+"./request COMMAND ..." - simple hook to send a request to the ratelimit-server
+e.g.
+	./request get_size
+
+Slightly less crap version:
+"./mb_server/bin/ratelimit-client ADDR PORT QUERY"
+e.g.
+	"./mb_server/bin/ratelimit-client 10.1.1.245 2000 get_size"
+
+snmp
+====
+
+http://stats.musicbrainz.org/mrtg/drraw/drraw.cgi?Mode=view;Dashboard=1262470284.29707
+
+From left to right:
+
+- number of keys.  Not amazingly useful.  This is just the size of the
+  in-memory Perl hash in the ratelimit-server.  Starts at zero when the
+  ratelimit-server (re-)starts; grows.
+  (Think: scalar keys %hash)
+
+- bucket spread.  Represents the percentage of buckets used in the above hash.
+  (Think: scalar %hash)
+
+- "ws global" requests.  Currently shows requests/refusals per second.
+  Ideally of course refusals should be zero.
+
+- "ws global" peak rate.  This is the peak rate per time-period.  We currently
+  have ws-global set to 900 per 10 seconds, so the max rate is 900.  If this
+  is constantly pegged at 900, it means that (during every 5-minute interval)
+  we hit the 900 limit at least once.
+
+So in summary:
+
+- keys: pretty much ignore
+- spread: 100% good, 0% bad
+- ws global requests:
+  - requests represents our traffic
+  - refusals: 0 good, higher=bad
+- ws global peak rate:
+  - pegged at our configured limit = bad, lower=good
+
+Still plenty of work to be done to get that packaged up, to get it to work
+across carl too, etc.
+
+TODO:
+
+- have snmpd on carl/lenny listen on specific IPs, not 0.0.0.0
+- have scooby mrtg monitor snmp on ratelimit.localdomain
+?
+
+- package it up
+
---
+++ snmp-agent.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl
+# vi: set ts=4 sw=4 :
+
+use warnings;
+use strict;
+
+use lib "./mb_server/cgi-bin";
+use MusicBrainz::Server::RateLimitClient;
+use SNMPAgentUtil;
+
+# exec "sudo", "-u", "exim", $0, @ARGV unless getpwnam($<) eq "exim";
+# pass_persist .1.3.6.1.4.1.1532281856.1 /usr/local/bin/linux26-diskstats-agent
+
+if ("@ARGV" eq "dump")
+{
+	my @r;
+	sub M::set_responses { shift; @r = @_; }
+	prepare_responses("M");
+	while (@r)
+	{
+		my ($oid, $type, $value) = splice(@r, 0, 3);
+		printf "$oid\t$type\t$value\n";
+	}
+	exit;
+}
+
+require SNMPAgentUtil;
+
+SNMPAgentUtil->run(
+	# log_to => "/dev/tty",
+	# log_to => "/tmp/exim_snmp_agent.log",
+	# get => \&get,
+	# get_next => \&get_next,
+	prepare_responses => \&prepare_responses,
+);
+
+{
+    my $expire = time;
+
+    sub prepare_responses
+    {
+        my ($self) = @_;
+
+        return if defined($expire) and time() < $expire;
+
+        my @responses;
+
+	# .1.3.6.1.4.1.1532281856 = fake djce enterprise
+	#	.77 = ratelimit-server stats
+	#	  .1.X = name (string)
+	#	  .2.X = keys (gauge)
+	#	  .3.X = key hash ratio (0-100) (gauge)
+	#	  .4.1.X = instance ID (integer)
+	#	  .4.2.X = key (string)
+	#	  .4.3.X = requests (counter)
+	#	  .4.4.X = refusals (counter)
+	#	  .4.5.X = last max rate (gauge)
+
+	my $base = ".1.3.6.1.4.1.1532281856.77";
+
+	my $instance_num = 0;
+	my $key_num = 0;
+
+	my $do = sub {
+		my ($name, $addr, $port, $keys) = @_;
+		++$instance_num;
+
+		push @responses, "$base.1.$instance_num", "string", $name;
+
+		{
+			my $t = MusicBrainz::Server::RateLimitClient->query("get_size", "${addr}:${port}")
+				or last;
+			$t =~ m[\bkeys=(\d+)]
+				and push @responses, "$base.2.$instance_num", "gauge", $1;
+			$t =~ m[\bsize=(\d+)\/(\d+)]
+				and push @responses, "$base.3.$instance_num", "gauge", int(0.5 + 100*$1/$2);
+		}
+
+		for my $key (@$keys)
+		{
+			++$key_num;
+			push @responses, "$base.4.1.$key_num", "integer", $instance_num;
+			push @responses, "$base.4.2.$key_num", "string", $key;
+
+			my $t = MusicBrainz::Server::RateLimitClient->query("get_stats $key", "${addr}:${port}")
+				or next;
+			$t =~ m[\bn_req=(\d+)]
+				and push @responses, "$base.4.3.$key_num", "counter", $1;
+			$t =~ m[\bn_over=(\d+)]
+				and push @responses, "$base.4.4.$key_num", "counter", $1;
+			$t =~ m[\blast_max_rate=(\d+)]
+				and push @responses, "$base.4.5.$key_num", "gauge", $1;
+		}
+	};
+
+	&$do("default", "10.1.1.245", 2000, ["ws global", "ws ua=-", "ws ua=libvlc", "ws ua=nsplayer", "ws cust=bbc",
+		"ws ua=python-musicbrainz/0.7.3",
+		"ws ua=generic-bad-ua",
+		]);
+
+        $self->set_responses(@responses);
+
+        $expire = time() + 10;
+    }
+}
+
+# eof
