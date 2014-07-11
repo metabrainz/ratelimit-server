791dce1ee284ca4dad6e23555790807d59db9dcd
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-07-11 07:21:58 -0700

    up ratelimit for 7d

--- RateLimitServer.pm
+++ RateLimitServer.pm
@@ -229,6 +229,7 @@ sub fixup_key
 	$key =~ s/\bip=(132\.185\.\d+\.\d+)\b/cust=bbc/;
 	$key =~ s/\bip=(212\.58\.2[2-5]\d\.\d+)\b/cust=bbc/;
 	$key =~ s/\bip=(207\.241\.2(2[4-9]|3[0-9])\.\d+)\b/cust=ia/;
+	$key =~ s/\bip=(84\.45\.16\.4|81\.153\.103\.174)\b/cust=7d/;
 
 	$key =~ s{ ua=([ -]*|((Java|Python-urllib|Jakarta Commons-HttpClient)/[0-9._]+))$}{ ua=generic-bad-ua}
 		unless $key =~ m{\Q ua=python-musicbrainz/0.7.3\E};
@@ -257,6 +258,7 @@ my $processors = [
     # BBC/IA first, make sure their IPs get the high ratelimit
     {match => qr{^([^\s]*) cust=bbc$}, limit => 15*20, period => 20, stats => 1},
     {match => qr{^([^\s]*) cust=ia$}, limit => 220, period => 20, stats => 1},
+    {match => qr{^([^\s]*) cust=7d$}, limit => 220, period => 20, stats => 1},
     # Per-user ratelimits (strict)
     {match => qr{^frontend ip=(\d+\.\d+\.\d+\.\d+)$}, limit => 45, period => 20, strict => 1},
     {match => qr{^ws ip=(\d+\.\d+\.\d+\.\d+)$}, limit => 22, period => 20, strict => 1},
